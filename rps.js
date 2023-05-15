fa60620879e7bd4744825a5a74d455114e6d5e62
Lizzy <nynjalizzy@gmail.com>
2023-05-15 12:46:18 -0700

    Display round result in window

--- rps.js
+++ rps.js
@@ -1,7 +1,7 @@
 const rockBtn = document.getElementById('rock');
 const paperBtn = document.getElementById('paper');
 const scissorsBtn = document.getElementById('scissors');
-const buttons = document.querySelectorAll('button');
+let result = document.querySelector('.result');
 
 const choices = ["Rock", "Paper", "Scissors"]
 
@@ -19,38 +19,38 @@ function round(playerChoice) {
     console.log("Computer choice: ", computerChoice)
     if (playerChoice.toLowerCase() == "rock") {
         if (computerChoice == "Scissors") {
-            console.log("Rock crushes scissors. You win!")
+            result.textContent = "Rock crushes scissors. You win!";
             return score++;
         }
         else if (computerChoice == "Paper") {
-            console.log("Paper covers rock. You lose!")
+            result.textContent = "Paper covers rock. You lose!";
             return score--;
         }
-        else console.log ("Tie!")
+        else result.textContent = "Tie!";
             return score;
     }
     if (playerChoice.toLowerCase() == "paper") {
         if (computerChoice == "Rock") {
-            console.log ("Paper covers rock. You win!")
+            result.textContent = "Paper covers rock. You win!";
             return score++;
         }
         else if (computerChoice == "Scissors") {
-            console.log ("Scissors cut paper. You lose!")
+            result.textContent = "Scissors cut paper. You lose!";
             return score--;
         }
-        else console.log ("Tie!")
+        else result.textContent = "Tie!";
             return score;
     }
     if (playerChoice.toLowerCase() == "scissors") {
         if (computerChoice == "Rock") {
-            console.log ("Rock crushes scissors. You lose!")
+            result.textContent = "Rock crushes scissors. You lose!";
             return score--;
         }
         else if (computerChoice == "Paper") {
-            console.log ("Scissors cut paper. You win!")
+            result.textContent = "Scissors cut paper. You win!";
             return score++;
         }
-        else console.log ("Tie!")
+        else result.textContent = "Tie!";
             return score;
     }
 }
